db0af6ad41229766604a573b38bae520a1d671f3
Pohaku808 <d_man_808@icloud.com>
2024-02-13 18:27:19 -0700

    Minesweeper Game Module (Damien Padilla, 12/13/24)

--- MilestoneCST-350(Damien)/Controllers/MinesweeperController.cs
+++ MilestoneCST-350(Damien)/Controllers/MinesweeperController.cs
@@ -5,26 +5,50 @@ namespace MilestoneCST_350_Damien_.Controllers
 {
 	public class MinesweeperController : Controller
 	{
-		public int BoardSize = 8;
-		//NOTE: There could be error here
-		private double BoardDifficulty = 0.05;
+		public static int BoardSize = 8;
 
+		public static GameBoardModel board = new GameBoardModel(BoardSize);
+
+		// You can only manually change the difficulty of the game as of right now
+		private double BoardDifficulty = 0.06;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		public IActionResult Index()
 		{
-			return View();
-		}
+			//set bombs up on board
+            board.SetUpBombs(BoardDifficulty);
 
-		public IActionResult DisplayMinesweeperGame()
+			// calcularte live neighbors on board
+            board.CalculateLiveNeighbors();
+
+			return View(board);
+        }
+
+
+		/// <summary>
+		/// When a button is clicked mark it as visited and flood fill board
+		/// </summary>
+		/// <param name="clickedCell"></param>
+		/// <returns>GameBoardModel</returns>
+		public IActionResult HandleButtonClick(string clickedCell)
 		{
-			GameBoardModel board = new GameBoardModel(BoardSize);
+			string[] parts = clickedCell.Split(',');
 
-			board.SetUpBombs(BoardDifficulty);
+			int row = Int32.Parse(parts[0]);
 
-			board.CalculateLiveNeighbors();
+			int col = Int32.Parse(parts[1]);
 
-			return View("Minesweeper", board);
-			//return minesweeper view
+			board.Grid[row,col].Visited = true;
+
+			board.Fill(row, col);
+
+
+			return View("Index", board);
 		}
+
 
 	}
 }
