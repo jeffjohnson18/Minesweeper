ef8ce924dba1b65d86f0682ca6f6d01bc68e1d52
jeffrey johnson <jeffrey070803@gmail.com>
2024-04-12 11:27:39 -0700

    including state variables

--- MilestoneCST-350(Damien)/Controllers/LoginController.cs
+++ MilestoneCST-350(Damien)/Controllers/LoginController.cs
@@ -4,43 +4,54 @@ using MilestoneCST_350_Damien_.Services.Business;
 
 namespace MilestoneCST_350_Damien_.Controllers
 {
-    public class LoginController : Controller
-    {
-        /// <summary>
-        /// Login Page
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class LoginController : Controller
+	{
+		private readonly IHttpContextAccessor _context;
+		/// <summary>
+		/// Login Page
+		/// </summary>
+		/// <returns></returns>
+		/// 
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        /// <summary>
-        /// Authenticate the users login info
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        public IActionResult ProcessLogin(UserModel user)
-        {
-            // Instantiate the SecurityServices class
-            SecurityService securityService = new SecurityService();
+		// Constructor to inject dependency variable (context)
+		public LoginController(IHttpContextAccessor context)
+		{
+			_context = context;
+		}
 
-            // Check if user is valid
-            int userId = securityService.IsValid(user);
+		/// <summary>
+		/// Authenticate the users login info
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public IActionResult ProcessLogin(UserModel user)
+		{
+			// Instantiate the SecurityServices class
+			SecurityService securityService = new SecurityService();
+
+			// Check if user is valid
+			int userId = securityService.IsValid(user);
 
 			// User the object to call the method IsValid
 			if (userId != -1)
-            {
-                // Redirect to Difficulty controller and also send the current logged in user's id
-                return RedirectToAction("Index", "Difficulty", new { userId = userId });
-            }
-            else
-            {
+			{
+				// verify the state 
+				_context.HttpContext.Session.SetString("UserName", user.UserName);
 
-                return View("LoginFailure", user);
-            }
-        }
+				// Redirect to Difficulty controller and also send the current logged in user's id
+				return RedirectToAction("Index", "Difficulty", new { userId = userId });
+			}
+			else
+			{
+
+				return View("LoginFailure", user);
+			}
+		}
 
 
-    }
+	}
 }
